405c018a1613b36b2b9e84fb24137f75128bb844
Sander Meester <sander_mr@hotmail.com>
2018-09-06 18:16:41 +0200

    until 7 done, started with 8

--- sander_meester/week1/Sander_Meester_11014822_Lab1.hs
+++ sander_meester/week1/Sander_Meester_11014822_Lab1.hs
@@ -9,6 +9,7 @@
 module Lab1 where
 import Data.List
 import Test.QuickCheck
+import Data.Char
 
 prime :: Int -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
@@ -86,7 +87,7 @@ reversablePrimes = filter (isReversablePrime) (findPrimes 10000)
 -- Source of slice:
 -- https://www.google.nl/search?q=haskell+slice+list&oq=haskell+slice&aqs=chrome.0.69i59j69i60j0j69i57j0l2.1217j0j7&sourceid=chrome&ie=UTF-8
 
--- time:
+-- time: 45 mins
 slice :: Int -> Int -> [Int] -> [Int]
 slice from to xs = take (to - from + 1) (drop from xs)
 
@@ -97,4 +98,60 @@ getSumOfPrimes i j
 
 get101Prime = getSumOfPrimes 0 100
 
--- We can test this by going over all primes starting at 100, and checking whether it is a sum of the 100 primes before it
+-- We can test this by going down over all primes starting at the prime we had as an answer, and checking whether it is a sum of any of sets of 100 consecutive primes found before this prime.
+
+--Exercise 6
+-- time 30 mins
+getProductOfPrimes :: Int -> Int
+getProductOfPrimes i
+    | not (prime(product (slice 0 i primes ) +1)) = primes !! i
+    | otherwise = getProductOfPrimes (i+1)
+
+getCounterExample = getProductOfPrimes 2
+-- The smallest counterexample is [2*3*5*7*11*13]+1 = 30031 (Pn=13)
+
+--Exercise 7
+-- time 1 hour (- tests)
+-- https://stackoverflow.com/questions/2838727/how-do-i-get-the-sums-of-the-digits-of-a-large-number-in-haskell
+sumDigit :: Int -> Int
+sumDigit 0 = 0
+sumDigit x = (x `mod` 10) + sumDigit (x `div` 10)
+
+doubleSecondDigits :: [Int] -> [Int]
+doubleSecondDigits (n:nt:ns) = n : sumDigit(nt*2) : doubleSecondDigits(ns)
+doubleSecondDigits n = n
+
+luhn :: Integer -> Bool
+luhn n = sum (doubleSecondDigits (map digitToInt (show n))) `mod` 10 == 0
+
+isAmericanExpress, isMaster, isVisa :: Integer -> Bool
+isAmericanExpress n = luhn n
+isMaster n = luhn n
+isVisa n = luhn n
+
+-- Test functions:
+-- TODO: these
+
+-- Exercise 8
+--  time
+
+accuses :: Boy -> Boy -> Bool
+accuses b1 b2
+    | b1 == Peter && (b2 == Matthew || b2 == Jack) = True
+    | b1 == Jack && (b2 == Matthew || b2 == Peter) = True
+    | b1 == Carl && b2 == Arnold = True
+    -- | b1 == Arnold && (b2 == Matthew || b2 == Peter) = True
+    | otherwise = False
+
+accusers :: Boy -> [Boy]
+accusers b
+    | b == Matthew = [Peter, Jack]
+    | b == Peter = [Jack]
+    | b == Jack = [Peter]
+    | b == Arnold = [Carl]
+    | b == Carl = []
+    | otherwise = []
+
+
+-- guilty :: [Boy]
+-- honest :: [Boy]
