6f4cbb97f36bad818f482942e9f25b19f7b7d944
sambous <gsamyb@gmail.com>
2018-09-11 21:25:32 +0200

    Renaming and refactoring, 8 almost done

--- SamyBoussaa/Samy_Boussaa_Lab1.hs
+++ SamyBoussaa/Samy_Boussaa_Lab1.hs
@@ -1,11 +1,14 @@
+
 import Test.QuickCheck
 import Data.List
 import Lab1
+import Data.Char
 -- import Data.Numbers
 
 -- Assignment 1 formula 2
 -- started 9:15
 -- finished 10:17
+
 leftHandSide:: Int -> Int
 leftHandSide n = sum(map (^2) [1..n])
 
@@ -71,6 +74,8 @@ assignThree n = length (permutations [1..n]) == factorial n
 -- Is the property hard to test? If you find that it is, can you given a reason why?
 
 
+
+
 -- Assignment 4
 -- started 12.12
 -- finished ...
@@ -89,7 +94,7 @@ testPrimeListReversal = ((sort [ reversal x | x<-primeListReversal]) == primeLis
 -- finished 15:35
 
 -- 37447
-
+-- https://stackoverflow.com/questions/4597820/does-haskell-have-list-slices-i-e-python
 slice :: Int -> Int -> [Integer] -> [Integer]
 slice from to xs = take (to - from) (drop from xs)
 
@@ -112,29 +117,73 @@ conjuctureSix n total
 conjuctureSixMin :: [Integer]
 conjuctureSixMin = take 1 (conjuctureSix 0 [])
 
-
-
--- Old implementation
--- if not (prime (product(slice 0 n primes) + 1))
---                 then  "When n is " ++ show n ++ " then the number that is not prime is " ++ show (product(slice 0 n primes) + 1) ++
---                       ""
---                 else conjuctureSix (n + 1)
-
-
-
- -- Assignment 7 - read, string == list of chars
+ -- Assignment 7
  -- started 17:37
  -- finished
-
+-- estimated time ~ 3 hours
 -- https://stackoverflow.com/questions/3963269/split-a-number-into-its-digits-with-haskell
 digits :: Integer -> [Int]
 digits n = map (\x -> read [x] :: Int) (show n)
 
-oneList :: [a] -> [b] -> [(a, b)]
-oneList []     _      = []
-oneList (x:xs) (y:ys) = (x, y) : oneList xs ys
+createTable :: Integer -> [(Int, Integer)]
+createTable creditcard_number = zip (digits creditcard_number) (combination)
+              where
+              combination = genericTake (genericLength (digits creditcard_number)) (cycle [2, 1])
 
-luhn :: Integer -> [(Int, Integer)]
-luhn creditcard_number = oneList digits creditcard_number combination
-                         where
-                        combination = genericTake (genericLength (digits creditcard_number)) (cycle [2, 1])
+doArith :: [(Int, Integer)] -> Int -> Int
+doArith [] total = last (digits (toInteger total))
+doArith tableCreditcardNumber total = if (length (digits (toInteger prodTuple)) == 2) then
+                                      doArith (tail tableCreditcardNumber) (total + sumTuple)
+                                      else
+                                      doArith (tail tableCreditcardNumber) (total + prodTuple)
+                                      where
+                                      prodTuple =  (product(head (map (\(x,y) -> [( x), (fromIntegral y)]) ([head tableCreditcardNumber]))))
+                                      sumTuple = (sum(head (map (\(x,y) -> [( x), (fromIntegral y)]) ([head tableCreditcardNumber]))))
+
+luhn :: Integer -> Bool
+luhn creditcard_number
+  | (doArith (createTable creditcard_number) 0) == 0 = True
+  | otherwise = False
+
+-- isMaster, isVisa
+-- https://en.wikipedia.org/wiki/Payment_card_number#cite_note-GenCardFeatures-6
+isAmericanExpress  :: Integer -> Bool
+isAmericanExpress creditcard_number = (length listNumbers == 15) &&
+                                      (((take 2 listNumbers) == [3,4]) || (take 2 listNumbers) == [3,7])
+                                      where
+                                      listNumbers = digits (toInteger creditcard_number)
+
+isMaster  :: Integer -> Bool
+isMaster creditcard_number = (length listNumbers == 16) &&
+                             ((firstTwo > 50  && firstTwo < 56) ||
+                             (firstFour > 2221  && firstTwo < 2720))
+                             where
+                             firstFour =  read (take 4 (show creditcard_number))
+                             firstTwo =  read (take 2 (show creditcard_number))
+                             listNumbers = digits (toInteger creditcard_number)
+isVisa  :: Integer -> Bool
+isVisa creditcard_number =  (head listNumbers == 4) && (length listNumbers == 16)
+                            where
+                            listNumbers = digits (toInteger creditcard_number)
+
+
+-- Assignment 8
+-- started 18:40
+-- finished
+-- estimated time 1.5 hours
+
+accuses :: Boy -> Boy -> Bool
+accuses boyOne boyTwo
+        |boyOne == Matthew = (boyTwo /= Carl && boyTwo /= Matthew)
+        |boyOne == Peter = (boyTwo == Jack || boyTwo == Matthew)
+        |boyOne == Jack =  (not (accuses Matthew boyTwo) &&  not (accuses Peter boyTwo))
+        |boyOne == Arnold = (accuses Matthew boyTwo || accuses Peter boyTwo)
+        |boyOne == Carl = not (accuses Arnold boyTwo)
+        |otherwise = False
+
+
+accusers :: Boy -> [Boy]
+accusers boy = [x | x<-boys, accuses x boy]
+
+
+-- Not done yet
