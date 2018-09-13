b0ac8a29ab2cd595ad9fb65953177e57b8d39e82
Simon <SJ.Baars@student.han.nl>
2018-09-13 18:33:48 +0200

    Finalized tests for assignment 2

--- 12072931 - Simon Baars/Lab 2/Main.hs
+++ 12072931 - Simon Baars/Lab 2/Main.hs
@@ -40,13 +40,23 @@ genBigNumbers = abs `fmap` (arbitrary :: Gen Int) `suchThat` (>1000)
 --
 --      Testing Equilateral by trying different kinds of cases that should return in Equilateral:
 --            triangle 10 10 10 --> Equilateral
---            triangle 100 100 100 --> Equilateral
---            triangle 1000 1000 1000 --> Equilateral
 --
 --      Testing Rectangular by trying different kinds of cases that should return in Rectangular (making sure I hit all parts of the Rectangular pattern's condition):
 --            triangle 3 4 5 --> Rectangular
 --            triangle 5 3 4 --> Rectangular
 --            triangle 4 5 3 --> Rectangular
+--
+--      Testing Isosceles by trying different kinds of cases that should return in Isosceles (making sure I hit all parts of the Isosceles pattern's condition):
+--            triangle 20 15 15 --> Isosceles
+--            triangle 15 20 15 --> Isosceles
+--            triangle 15 15 20 --> Isosceles
+--
+--      Testing Other by trying different kinds of cases that should return in Isosceles (making sure I hit all parts of the Isosceles pattern's condition):
+--            triangle 10 11 12 --> Other
+--            triangle 12 10 11 --> Other
+--            triangle 11 12 10 --> Other
+--
+-- All these properties are being tested for by the function `testTriangle`.
 
 isPythagorean :: Integer -> Integer -> Integer -> Bool
 isPythagorean a b c = a^2 + b^2 == c^2
@@ -66,6 +76,19 @@ triangle x y z = case triangleType (x,y,z) of "Not a triangle" -> NoTriangle
                                               "Isosceles" -> Isosceles
                                               _ -> Other
 
+checkTriangleFunction :: Integer -> Integer -> Integer -> Shape -> IO ()
+checkTriangleFunction x y z expectedShape = if (triangle x y z == expectedShape) && (triangle z x y == expectedShape) && (triangle y z x == expectedShape)
+  then putStrLn("Tests for shape " ++ show expectedShape ++ " succeeded for input variables "++show x++", "++show y++", "++show z)
+  else error("Tests for shape " ++ show expectedShape ++ " FAILED for input variables "++show x++", "++show y++", "++show z)
+
+testTriangle :: IO ()
+testTriangle = do
+  checkTriangleFunction 90 10 10 NoTriangle
+  checkTriangleFunction 10 10 10 Equilateral
+  checkTriangleFunction 3 4 5 Rectangular
+  checkTriangleFunction 20 15 15 Isosceles
+  checkTriangleFunction 10 11 12 Other
+
 checkTestResult :: Bool -> String
 checkTestResult True  = "Test succeeded!"
 checkTestResult False = "Test failed!"
@@ -76,7 +99,7 @@ main = do
   testQuartilesSafe 10000 >>= \y -> putStrLn("Safe, but without QuickCheck: " ++ checkTestResult y)
   quickCheckResult $ forAll genBigNumbers testQuartiles
 
-  putStrLn "\n== Assignment 1 =="
-  putStrLn $ triangleType (100,41,39)
+  putStrLn "\n== Assignment 2 =="
+  testTriangle
 
   putStrLn "Done"
