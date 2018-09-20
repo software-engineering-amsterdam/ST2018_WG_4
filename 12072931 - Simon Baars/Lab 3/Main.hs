38b8d28a876f68ffaa17265496a479b0f8251b32
Simon <SJ.Baars@student.han.nl>
2018-09-20 16:07:00 +0200

    Created the auto form generator
    But haskell optimizes it and it breaks :(

--- 12072931 - Simon Baars/Lab 3/Main.hs
+++ 12072931 - Simon Baars/Lab 3/Main.hs
@@ -3,6 +3,8 @@ import Lecture3
 import Data.List
 import Data.Char
 import Test.QuickCheck
+import System.IO.Unsafe
+import System.Random
 
 -- Assignment 1 (Definitions of contradiction, tautology, entails and equiv)
 -- Time: 1.5 hours
@@ -52,6 +54,45 @@ pOrQ = Dsj[p,q]
 pAndQ = Cnj[p,q]
 
 -- Assignment 2 (Testing the propositional formula parser)
+
+--chooseTreeOption :: (Int -> Form) -> Int -> Property
+--chooseTreeOption function maximumNumber = forAll (resize 1 (abs `fmap` (arbitrary :: Gen Int) `suchThat` (>maximumNumber-2))) (\x -> function (x `mod` maximumNumber))
+
+data TreeState = TreeState {amountOfProperties :: Int, form :: Form}
+
+chooseTreeOption :: Int -> Int
+chooseTreeOption maximumNumber = unsafePerformIO (getStdRandom(randomR (0,maximumNumber-1)))
+
+maxTreeChance = 10 -- This number decides the size of the resulting form.
+
+generateActualForm :: Form
+generateActualForm = form (generateForm (maxTreeChance-1) (TreeState 1 p)) -- This number decides the maximum amount of subforms in the resulting form.
+
+generateForm :: Int -> TreeState -> TreeState
+generateForm leftTreeChance state = if chooseTreeOption maxTreeChance < leftTreeChance then chooseRandomSplitForm (leftTreeChance - 1) state else chooseRandomProperty state
+
+chooseRandomSplitForm :: Int -> TreeState -> TreeState
+chooseRandomSplitForm leftTreeChance state = case chooseTreeOption 2 of 0 -> let x = generateFormList leftTreeChance state maxTreeChance []
+                                                                             in if chooseTreeOption 2 == 0 then treeStateListToTreeState Cnj x
+                                                                                                           else treeStateListToTreeState Dsj x
+                                                                        1 -> let x = generateForm leftTreeChance state
+                                                                                 y = generateForm leftTreeChance x
+                                                                             in if chooseTreeOption 2 == 0 then TreeState (amountOfProperties y) (Impl (form x) (form y))
+                                                                                                           else TreeState (amountOfProperties y) (Equiv (form x) (form y))
+
+treeStateListToTreeState :: ([Form] -> Form) -> [TreeState] -> TreeState
+treeStateListToTreeState f states = TreeState (amountOfProperties (last states)) (f (map form states))
+
+generateFormList :: Int -> TreeState -> Int -> [TreeState] -> [TreeState]
+generateFormList leftTreeChance state maxListSize currentFormList = case chooseTreeOption 2 of 0 -> generatedState:currentFormList
+                                                                                               1 -> generatedState:generateFormList leftTreeChance generatedState (maxListSize-1) currentFormList
+                                                                                               _ -> error "Something impossible just happened!"
+                                                            where generatedState = generateForm leftTreeChance state
+
+chooseRandomProperty :: TreeState -> TreeState
+chooseRandomProperty state = TreeState (if chosenProperty == props then props + 1 else props) (Prop chosenProperty)
+  where props = amountOfProperties state
+        chosenProperty = chooseTreeOption (props + 1)
 
 checkTestResult :: Bool -> String
 checkTestResult True  = "\x1b[32mTest succeeded!\x1b[0m"
