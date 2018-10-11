2497591e633e9e9d1f7b3f34af451888709d3439
Simon <SJ.Baars@student.han.nl>
2018-10-11 11:51:19 +0200

    Lab 6 Assignment 1

---
+++ 12072931 - Simon Baars/Workshop6/Main.hs
@@ -0,0 +1,60 @@
+module Main where
+
+import Data.Char
+import Data.List
+
+-- Question 1
+data Blt a = Leaf a | Node (Blt a) (Blt a) deriving (Eq,Show)
+
+exampleTree :: Blt String
+exampleTree = Node (Node (Leaf "Hoare, Tony")
+                         (Leaf "Turing, Alan"))
+                   (Leaf "Goedel, Kurt")
+
+leafCount :: Blt a -> Int
+leafCount (Leaf node) = 1
+leafCount (Node node1 node2) = leafCount node1 + leafCount node2
+
+-- Question 2
+mapB :: (a -> b) -> Blt a -> Blt b
+mapB f (Leaf node) = Leaf (f node)
+mapB f (Node node1 node2) = Node (mapB f node1) (mapB f node2)
+
+-- Question 3
+data Tree a = T a [Tree a] deriving (Eq,Ord,Show)
+
+example1 = T 1 [T 2 [], T 3 []]
+example2 = T 0 [example1,example1,example1]
+
+count :: Tree a -> Int
+count (T x nodes) = length nodes + foldr (\x acc -> acc + count x) 0 nodes
+
+-- Question 4
+depth :: Tree a -> Int
+depth (T _ []) = 0
+depth (T _ ts) = foldl max 0 (map depth ts) + 1
+
+-- Question 5
+mapT :: (a -> b) -> Tree a -> Tree b
+mapT f (T x nodes) = T (f x) (map (mapT f) nodes)
+
+-- Question 7
+collect :: Tree a -> [a]
+collect (T x nodes) = x : foldr (\x acc -> collect x++acc) [] nodes
+
+-- Question 8
+foldT :: (a -> [b] -> b) -> Tree a -> b
+foldT f (T x ts) = f x (map (foldT f) ts)
+
+count' :: Tree a -> Int
+count' x = foldT (\x y -> 1 + sum y) x - 1
+
+depth' :: Tree a -> Int
+depth' (T _ []) = 0
+depth' (T _ ts) = foldl max 0 (map depth ts) + 1
+
+collect' :: Tree a -> [a]
+collect' (T x nodes) = x : foldr (\x acc -> collect x++acc) [] nodes
+
+main :: IO ()
+main = putStrLn "Example tree size: "
